9170be832c4c5d6ae86d5b3d39a5227750862720
Kirill <kirillgaynov@gmail.com>
2024-01-18 15:39:13 +0300

    added decorator for tests using random module

--- lesson_13/unit_test.py
+++ lesson_13/unit_test.py
@@ -3,6 +3,17 @@ from sorting import bubble_sort
 import random
 
 
+def repeat_test(times):
+    def repeat(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                func(*args, **kwargs)
+
+        return wrapper
+
+    return repeat
+
+
 class TestStringMethods(unittest.TestCase):
     """
     Тест пузырьковой сортировки
@@ -12,12 +23,15 @@ class TestStringMethods(unittest.TestCase):
     2. уже отсортированный список в обратном порядке
     3. список в котором одинаковые значения
     4. Уже отсортированный список
+    5. пустой список
     """
 
+    @repeat_test(times=5)
     def test_random_sample(self):
         test_arr = random.sample(range(-1000, 1000), 20)
         self.comparing(test_arr)
 
+    @repeat_test(times=5)
     def test_reverse_sorted_sample(self):
         test_arr = random.sample(range(-1000, 1000), 20)
         test_arr.sort()
@@ -28,8 +42,14 @@ class TestStringMethods(unittest.TestCase):
         test_arr = [0] * 10
         self.comparing(test_arr)
 
+    @repeat_test(times=5)
     def test_sorted_sample(self):
         test_arr = random.sample(range(-1000, 1000), 20)
+        test_arr.sort()
+        self.comparing(test_arr)
+
+    def test_empty_list(self):
+        test_arr = []
         test_arr.sort()
         self.comparing(test_arr)
 
