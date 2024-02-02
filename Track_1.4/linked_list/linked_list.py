7a0322052b0466b59564cd21bdb637ba64d838e7
Kirill <kirillgaynov@gmail.com>
2024-02-03 00:44:22 +0300

    Corrected delete method

--- Track_1.4/linked_list/linked_list.py
+++ Track_1.4/linked_list/linked_list.py
@@ -51,24 +51,41 @@ class LinkedList:
 
         while node is not None:
             if node.value == val:
-                if previous_node is None:
+                # if last value
+                if node.next is None:
                     # if first value
-                    self.head = node.next  # then we set the second value in the head
+                    if previous_node is None:
+                        # The list becomes empty
+                        self.head = None
+                        self.tail = None
+                    else:
+                        # if middle or last value
+                        previous_node.next = None  # Update tail
+                        self.tail = previous_node
                 else:
-                    # if middle or last value
-                    previous_node.next = node.next  # otherwise the preceding will be referenced to the following
-                del node  # delete current
+                    # if first value
+                    if previous_node is None:
+                        self.head = node.next  # then we set the second value in the head
+                    else:
+                        # if middle or last value
+                        previous_node.next = node.next
+
+                if not all:
+                    break
+
                 if previous_node is None:
                     # if first value
                     node = self.head  # then the new node will be the head
                 else:
                     # if middle or last value
                     node = previous_node.next  # otherwise the new node becomes the following value
-                if not all:
-                    break
+
             else:
                 # if don't equal to desired
                 previous_node, node = node, node.next  # previous becomes current, current becomes next
+
+    if self.tail and self.tail.value == val:
+        self.tail = previous_node
 
     def clean(self):
         if self.head is None:
