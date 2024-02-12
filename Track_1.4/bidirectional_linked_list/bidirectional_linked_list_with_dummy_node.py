31ad7a2d18b0494f8a2db034c74b94af08ac7e02
Kirill <kirillgaynov@gmail.com>
2024-02-12 11:38:24 +0300

    added tests for dummy bdl and added some methods for bdl class

--- Track_1.4/bidirectional_linked_list/bidirectional_linked_list_with_dummy_node.py
+++ Track_1.4/bidirectional_linked_list/bidirectional_linked_list_with_dummy_node.py
@@ -1,9 +1,11 @@
-from bidirectional_linked_list import Node, LinkedList2
+from bidirectional_linked_list import Node
 
 
-class Dummy(LinkedList2):
+class Dummy:
+
     def __init__(self):
-        super().__init__(head=Node(None), tail=Node(None))  # dummy head and tail
+        self.head = Node(None)  # dummy head
+        self.tail = Node(None)  # dummy tail
         self.head.next = self.tail
         self.tail.prev = self.head
 
@@ -13,20 +15,61 @@ class Dummy(LinkedList2):
         self.tail.prev.next = item  # from penultimate to tail
         self.tail.prev = item  # new actual tail node
 
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
     def delete(self, value, all=False):
         current_node = self.head.next
-        while current_node.value is not None:
+        while current_node != self.tail:
             if current_node.value == value:
                 current_node.prev.next, current_node.next.prev = current_node.next, current_node.prev
                 if not all:
                     break
             current_node = current_node.next
 
+    def find(self, val):
+        node = self.head.next
+        while node != self.tail:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
 
-if __name__ == '__main__':
-    l = Dummy()
-    l.add_in_tail(Node(1))
-    l.add_in_tail(Node(2))
-    l.add_in_tail(Node(1))
-    l.delete(1, all=False)
-    l.print_all_nodes()
+    def find_all(self, val):
+        result = []
+        node = self.head.next
+        while node != self.tail:
+            if node.value == val:
+                result.append(node)
+            node = node.next
+
+        return result
+
+    def clean(self):
+        self.head.next = None
+        self.tail.next = None
+
+    def len(self):
+        length = 0
+        node = self.head.next
+        while node != self.tail:
+            length += 1
+            node = node.next
+        return length
+
+    def insert(self, afterNode, newNode):
+        if afterNode is None:  # problem statement
+            self.add_in_tail(newNode)
+        else:
+            newNode.prev = afterNode
+            newNode.next = afterNode.next
+            afterNode.next = newNode
+            newNode.next.prev = newNode
+        return
+
+    def add_in_head(self, newNode):
+        newNode.next, self.head.next.prev = self.head.next, newNode
+        self.head = newNode
---
+++ Track_1.4/bidirectional_linked_list/unit_test_dummy_bdl.py
@@ -0,0 +1,223 @@
+import unittest
+import random
+from bidirectional_linked_list import Node
+from bidirectional_linked_list_with_dummy_node import Dummy
+
+
+def repeat_test(times):
+    """
+    кастомный декоратор для повтора вызова функции times раз
+    """
+
+    def repeat(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                func(*args, **kwargs)
+
+        return wrapper
+
+    return repeat
+
+
+class UnitTestSortMethods(unittest.TestCase):
+    """
+    Тест методов связанного списка
+
+    Рассмотрим следующие состояния:
+    1. Один элемент
+    2. Много элементов в списке
+    3. Пустой список
+
+    И по качеству содержимого:
+    1. Одинаковые значения
+    2. Случайные значения
+    """
+
+    def test_delete_one_value_many_vals(self):
+        test_arr = [2] * 10
+        l_list = Dummy()
+
+        for item in test_arr:
+            l_list.add_in_tail(Node(item))
+
+        # Удаление 1 вхождения
+        l_list.delete(2, all=False)
+        test_arr.remove(2)
+
+        self.assertEqual(l_list.len(), len(test_arr))
+        self.assertEqual(l_list.head.next.value, 2)
+        self.assertEqual(l_list.tail.prev.value, 2)
+
+    def test_delete_all_value_many_vals(self):
+        test_arr = [2] * 10
+        l_list = Dummy()
+
+        for item in test_arr:
+            l_list.add_in_tail(Node(item))
+
+        # Удаление всех вхождений
+        l_list.delete(2, all=True)
+
+        test_arr = []
+        self.assertEqual(l_list.len(), len(test_arr))
+        self.assertEqual(l_list.head.next, l_list.tail)
+        self.assertEqual(l_list.tail.prev, l_list.head)
+
+    def test_delete_one_value_empty_list(self):
+        l_list = Dummy()
+
+        # Попытка удаления 1 вхождения
+        l_list.delete(2, all=False)
+
+        self.assertEqual(l_list.len(), 0)
+        self.assertEqual(l_list.head.next, l_list.tail)
+        self.assertEqual(l_list.tail.prev, l_list.head)
+
+    @repeat_test(times=3)
+    def test_delete_one_value_one_val(self):
+        test_arr = random.sample(range(-1000, 1000), 1)
+        value = random.choice(test_arr)
+        l_list = Dummy()
+
+        # Создание связанного списка
+        for item in test_arr:
+            l_list.add_in_tail(Node(item))
+
+        # Удаление первого вхождения
+        l_list.delete(value)
+        test_arr.remove(value)  # Удаляем первое вхождение
+
+        self.assertEqual(l_list.len(), len(test_arr))
+        self.assertEqual(l_list.head.next, l_list.tail)
+        self.assertEqual(l_list.tail.prev, l_list.head)
+
+    @repeat_test(times=3)
+    def test_delete_all_occurrences_many_values(self):
+        test_arr = random.sample(range(10, 20), 20)
+        value = random.choice(test_arr)
+        l_list = Dummy()
+
+        # Создание связанного списка
+        for item in test_arr:
+            l_list.add_in_tail(Node(item))
+
+        # Удаление всех вхождений
+        l_list.delete(value, all=True)
+
+        # Создание ожидаемой последовательности
+        expected_sequence = [item for item in test_arr if item != value]
+
+        # Проверка последовательности
+        node = l_list.head.next
+        for expected_value in expected_sequence:
+            self.assertEqual(node.value, expected_value)
+            node = node.next
+
+        self.assertEqual(l_list.len(), len(expected_sequence))
+        self.assertEqual(l_list.head.value, None)
+        self.assertEqual(l_list.tail.value, None)
+
+    @repeat_test(times=3)
+    def test_delete_all_occurrences_many_values(self):
+        expected_sequence = random.sample(range(-1000, 1000), 20)
+        value = random.choice(expected_sequence)
+        l_list = Dummy()
+
+        # Создание связанного списка
+        for item in expected_sequence:
+            l_list.add_in_tail(Node(item))
+
+        # Удаление 1 вхождения
+        l_list.delete(value, all=False)
+        # Создание ожидаемой последовательности
+        expected_sequence.remove(value)
+
+        # Проверка последовательности
+        node = l_list.head.next
+        for expected_value in expected_sequence:
+            self.assertEqual(node.value, expected_value)
+            node = node.next
+
+        self.assertEqual(l_list.len(), len(expected_sequence))
+        self.assertEqual(l_list.head.value, None)
+        self.assertEqual(l_list.tail.value, None)
+
+    def test_insert_in_empty_list(self):
+        l_list = Dummy()
+        value = 1
+
+        l_list.insert(None, Node(value))
+
+        self.assertEqual(l_list.len(), 1)
+        self.assertEqual(l_list.head.next.value, 1)
+        self.assertEqual(l_list.tail.prev.value, 1)
+
+    def test_insert_in_one_val_list(self):
+        l_list = Dummy()
+        node = Node(1)
+        value = 2
+        l_list.add_in_tail(node)
+        l_list.insert(node, Node(value))
+
+        node = l_list.head.next
+        for val in [1, 2]:
+            self.assertEqual(val, node.value)
+            node = node.next
+
+        self.assertEqual(l_list.len(), 2)
+        self.assertEqual(l_list.head.next.value, 1)
+        self.assertEqual(l_list.tail.prev.value, 2)
+
+    def test_insert_in_one_val_without_afternode(self):
+        l_list = Dummy()
+        node = Node(1)
+        value = 2
+        l_list.add_in_tail(node)
+        l_list.insert(None, Node(value))
+
+        node = l_list.head.next
+        for val in [1, 2]:
+            self.assertEqual(val, node.value)
+            node = node.next
+
+        self.assertEqual(l_list.len(), 2)
+        self.assertEqual(l_list.head.next.value, 1)
+        self.assertEqual(l_list.tail.prev.value, 2)
+
+    def test_insert_in_many_val_list(self):
+        l_list = Dummy()
+        l_list.add_in_tail(Node(1))
+        l_list.add_in_tail(Node(3))
+
+        value = 2
+        l_list.insert(l_list.head.next, Node(value))  # в середину
+
+        node = l_list.head.next
+        for val in [1, 2, 3]:
+            self.assertEqual(val, node.value)
+            node = node.next
+
+        self.assertEqual(l_list.len(), 3)
+        self.assertEqual(l_list.head.next.value, 1)
+        self.assertEqual(l_list.tail.prev.value, 3)
+
+    def test_insert_in_many_val_without_afternode(self):
+        l_list = Dummy()
+        l_list.add_in_tail(Node(1))
+        l_list.add_in_tail(Node(3))
+
+        value = 2
+        l_list.insert(None, Node(value))  # в конец
+
+        node = l_list.head.next
+        for val in [1, 3, 2]:
+            self.assertEqual(val, node.value)
+            node = node.next
+
+        self.assertEqual(l_list.len(), 3)
+        self.assertEqual(l_list.head.next.value, 1)
+        self.assertEqual(l_list.tail.prev.value, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
