d51edd3a99622635e908f4f3ab7042d007f9893a
Kirill <kirillgaynov@gmail.com>
2024-02-15 14:55:11 +0300

    fixed a potential error with extracting values from the stack during subtraction and division operations

--- Track_1.4/stack/calculate_postfix_expression.py
+++ Track_1.4/stack/calculate_postfix_expression.py
@@ -18,9 +18,13 @@ def calculate_postfix_expression(s: str) -> int:
         elif stack.size() == 2 and current_operation == '*':
             stack.push(stack.pop() * stack.pop())
         elif stack.size() == 2 and current_operation == '-':
-            stack.push(stack.pop() - stack.pop())
+            right_operand = stack.pop()
+            left_operand = stack.pop()
+            stack.push(left_operand - right_operand)
         elif stack.size() == 2 and current_operation in ['/', ':']:
-            stack.push(stack.pop() / stack.pop())
+            right_operand = stack.pop()
+            left_operand = stack.pop()
+            stack.push(left_operand / right_operand)
         elif stack.size() == 1 and current_operation is None:
             continue
         elif stack.size() == 1 and current_operation == '=':
