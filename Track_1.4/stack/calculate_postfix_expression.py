0008d5cdf2232e63f6570347b59fb16bc8f62d79
Kirill <kirillgaynov@gmail.com>
2024-02-15 13:58:36 +0300

    removed the try construct, replacing it with isdigit(), and added subtraction and division operators

--- Track_1.4/stack/calculate_postfix_expression.py
+++ Track_1.4/stack/calculate_postfix_expression.py
@@ -7,17 +7,21 @@ def calculate_postfix_expression(s: str) -> int:
     current_operation = None
 
     for char in s:
-        try:
+        if char.isdigit():
             char = int(char)
-        except ValueError:
-            current_operation = char
-            if stack.size() == 2 and current_operation == '+':
-                stack.push(stack.pop() + stack.pop())
-            elif stack.size() == 2 and current_operation == '*':
-                stack.push(stack.pop() * stack.pop())
-            elif stack.size() == 1 and current_operation is None:
-                continue
-            elif stack.size() == 1 and current_operation == '=':
-                return stack.pop()
-        else:
             stack.push(char)
+            continue
+
+        current_operation = char
+        if stack.size() == 2 and current_operation == '+':
+            stack.push(stack.pop() + stack.pop())
+        elif stack.size() == 2 and current_operation == '*':
+            stack.push(stack.pop() * stack.pop())
+        elif stack.size() == 2 and current_operation == '-':
+            stack.push(stack.pop() - stack.pop())
+        elif stack.size() == 2 and current_operation in ['/', ':']:
+            stack.push(stack.pop() / stack.pop())
+        elif stack.size() == 1 and current_operation is None:
+            continue
+        elif stack.size() == 1 and current_operation == '=':
+            return stack.pop()
