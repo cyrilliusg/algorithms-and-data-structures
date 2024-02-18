94ff3950ba7426397b71a64f6e82135d2deff2bf
Kirill <kirillgaynov@gmail.com>
2024-02-18 13:14:56 +0300

    test template

--- Track_1.4/queue/unit_test.py
+++ Track_1.4/queue/unit_test.py
@@ -1,115 +1,19 @@
 import unittest
-from reversed_stack import Stack, ReversedStack
 import random
+from queue import Queue
+from stack_queue import StackQueue
 
 
-class UnitTestDynArray(unittest.TestCase):
+class UnitTestQueue(unittest.TestCase):
     """
-    Тест методов стэка
+    Тест методов очереди
 
     Рассмотрим следующие случаи:
-    -- тестирование push
-    -- тестирование pop
-    -- тестирование peek
-    В Обычном и Реверснутом стэке
+    -- тестирование enqueue
+    -- тестирование dequeue
+    В Обычной очереди и очереди из 2-х Стэков
+    С пустой очередью, очередью из 1 элемента и множества элементов
     """
-
-    def test_push(self):
-        """
-        Проверка внесения значений
-        """
-        stack = Stack()
-        test_arr = random.sample(range(-10, 30), 10)
-
-        for item in test_arr:
-            stack.push(item)
-
-        for i in range(len(test_arr)):
-            self.assertEqual(test_arr[i], stack.stack[i])
-
-        self.assertEqual(len(test_arr), len(stack.stack))
-
-    def test_pop(self):
-        """
-        Проверка внесения значений
-        """
-        stack = Stack()
-        test_arr = random.sample(range(-10, 30), 10)
-
-        for item in test_arr:
-            stack.push(item)
-
-        test_arr.reverse()
-        for i in range(len(test_arr)):
-            self.assertEqual(test_arr[i], stack.pop())
-
-        self.assertEqual(0, len(stack.stack))
-
-    def test_peek(self):
-        """
-        Проверка внесения значений
-        """
-        stack = Stack()
-        test_arr = random.sample(range(-10, 30), 10)
-
-        for item in test_arr:
-            stack.push(item)
-
-        test_arr.reverse()
-
-        for i in range(len(test_arr)):
-            self.assertEqual(test_arr[i], stack.peek())
-            stack.pop()
-
-        self.assertEqual(0, len(stack.stack))
-
-    def test_reversed_push(self):
-        """
-        Проверка внесения значений
-        """
-        stack = ReversedStack()
-        test_arr = random.sample(range(-10, 30), 10)
-
-        for item in test_arr:
-            stack.push(item)
-
-        test_arr.reverse()
-
-        for i in range(len(test_arr)):
-            self.assertEqual(test_arr[i], stack.stack[i])
-
-        self.assertEqual(len(test_arr), len(stack.stack))
-
-    def test_reversed_pop(self):
-        """
-        Проверка внесения значений
-        """
-        stack = ReversedStack()
-        test_arr = random.sample(range(-10, 30), 10)
-
-        for item in test_arr:
-            stack.push(item)
-        test_arr.reverse()
-        for i in range(len(test_arr)):
-            self.assertEqual(test_arr[i], stack.pop())
-
-        self.assertEqual(0, len(stack.stack))
-
-    def test_reversed_peek(self):
-        """
-        Проверка внесения значений
-        """
-        stack = ReversedStack()
-        test_arr = random.sample(range(-10, 30), 10)
-
-        for item in test_arr:
-            stack.push(item)
-        test_arr.reverse()
-        for i in range(len(test_arr)):
-            self.assertEqual(test_arr[i], stack.peek())
-            stack.pop()
-
-        self.assertEqual(0, len(stack.stack))
 
 
 if __name__ == '__main__':
