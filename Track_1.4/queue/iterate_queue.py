5f032d53857eb0fa0593db0c636751c24586e776
Kirill <kirillgaynov@gmail.com>
2024-02-19 09:32:02 +0300

    fixed potential error

--- Track_1.4/queue/iterate_queue.py
+++ Track_1.4/queue/iterate_queue.py
@@ -3,8 +3,6 @@ from queue import Queue
 
 def rotate_queue(queue: Queue, n: int):
     size = queue.size()
-    # Ограничиваем n размером очереди, чтобы избежать лишних операций
-    n = n % size if size > 0 else 0
     for _ in range(n):
         item = queue.dequeue()
         queue.enqueue(item)
