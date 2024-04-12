02f0b046ef6b9384d095266855dc513cee2eccf1
Kirill <kirillgaynov@gmail.com>
2024-04-12 13:22:06 +0300

    removed default methods parameters

--- Track_1.6/recursion/recursion.py
+++ Track_1.6/recursion/recursion.py
@@ -27,7 +27,7 @@ def length_of_list(checkable_list: list) -> int:
     return 1 + length_of_list(checkable_list)  # прибавляем 1 + вызываем заново
 
 
-def is_palindrome(row: str, left_idx=0, right_idx=None) -> bool:
+def is_palindrome(row: str, left_idx, right_idx) -> bool:
     """Проверяет, является ли строка палиндромом, используя индексы для проверки символов."""
     if right_idx is None:
         right_idx = len(row) - 1  # Инициализируем правый индекс если он не установлен
@@ -44,7 +44,7 @@ def is_palindrome(row: str, left_idx=0, right_idx=None) -> bool:
     return is_palindrome(row, left_idx + 1, right_idx - 1)
 
 
-def print_even_numbers_only(list_of_nums: list, index=0) -> None:
+def print_even_numbers_only(list_of_nums: list, index) -> None:
     """Выводит на печать только чётные числа из списка, используя индекс для доступа к элементам."""
     if index >= len(list_of_nums):  # Базовый случай: если индекс вышел за пределы списка
         return
@@ -53,7 +53,7 @@ def print_even_numbers_only(list_of_nums: list, index=0) -> None:
     return print_even_numbers_only(list_of_nums, index + 1)
 
 
-def print_values_with_even_indexes(checkable_list: list, index=0) -> None:
+def print_values_with_even_indexes(checkable_list: list, index) -> None:
     """Выводит на печать только значения только с чётными индексами из списка"""
     if not checkable_list or index >= len(checkable_list):
         return
@@ -63,9 +63,9 @@ def print_values_with_even_indexes(checkable_list: list, index=0) -> None:
 
 
 def find_second_max_value_from_list(list_of_nums: list,
-                                    curr_idx: int = 0,
-                                    first_largest: Optional[int] = None,
-                                    second_largest: Optional[int] = None) -> Optional[int]:
+                                    curr_idx: int,
+                                    first_largest: Optional[int],
+                                    second_largest: Optional[int]) -> Optional[int]:
     """Возвращает второе по величине число из списка. Если список меньше двух значений, то вернёт None"""
     if len(list_of_nums) < 2:
         return None
@@ -87,7 +87,7 @@ def find_second_max_value_from_list(list_of_nums: list,
     return find_second_max_value_from_list(list_of_nums, curr_idx + 1, first_largest, second_largest)
 
 
-def find_files_in_directories(dir_path: str, files_names_list: Optional[list] = None) -> list:
+def find_files_in_directories(dir_path: str, files_names_list: Optional[list]) -> list:
     """Возвращает все файлы из текущего и вложенных каталогов по переданному пути"""
     if files_names_list is None:
         files_names_list = []
