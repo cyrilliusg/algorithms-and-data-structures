7505d024f3979d529d9efd0ad3345d6b59dec875
Kirill Gaynov <151780499+cyrilliusg@users.noreply.github.com>
2024-07-10 09:58:54 +0300

    Updated the KnuthSequence method to always generate at least the initial value of the sequence.

--- sorting/insertion_sorting/knuth_sequence.py
+++ sorting/insertion_sorting/knuth_sequence.py
@@ -1,7 +1,7 @@
 def KnuthSequence(array_size: int) -> list[int]:
     seq = []
     h = 1
-    while h < array_size:
+    while h < array_size or not seq:
         seq.insert(0, h)
         h = 3 * h + 1
     return seq
