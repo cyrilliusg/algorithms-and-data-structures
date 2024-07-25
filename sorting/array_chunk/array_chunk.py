b2c614355303352b7a1cd7174ced4c5bab74eaec
cyrilliusg <kirillgaynov@gmail.com>
2024-07-25 18:49:41 +0300

    fixed method logic

--- sorting/array_chunk/array_chunk.py
+++ sorting/array_chunk/array_chunk.py
@@ -1,28 +1,30 @@
 def ArrayChunk(M: list[int]) -> int:
-    arr_len = len(M)
-    N_index = arr_len // 2
-    N = M[N_index]
-    i1 = 0
-    i2 = arr_len - 1
-
     while True:
-        while M[i1] < N:
-            i1 += 1
-        while M[i2] > N:
-            i2 -= 1
+        N_index = len(M) // 2
+        N = M[N_index]
 
-        if i1 == i2 - 1 and M[i1] > M[i2]:
+        i1 = 0
+        i2 = len(M) - 1
+
+        while True:
+            while M[i1] < N:
+                i1 += 1
+
+            while M[i2] > N:
+                i2 -= 1
+
+            if i1 == i2 - 1 and M[i1] > M[i2]:
+                M[i1], M[i2] = M[i2], M[i1]
+                break
+
+            if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
+                return i2
+
+            if i1 == N_index:
+                N_index = i2
+            elif i2 == N_index:
+                N_index = i1
+
             M[i1], M[i2] = M[i2], M[i1]
-            continue
-
-        if i1 >= i2:
-            return N_index
-
-        M[i1], M[i2] = M[i2], M[i1]
-        if i1 == N_index:
-            N_index = i2
-        elif i2 == N_index:
-            N_index = i1
-
-        i1 += 1
-        i2 -= 1
+            i1 += 1
+            i2 -= 1
