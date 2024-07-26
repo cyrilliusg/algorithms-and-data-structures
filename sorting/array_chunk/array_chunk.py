440c0f090612e28bac23660ed367fe2932837000
cyrilliusg <kirillgaynov@gmail.com>
2024-07-26 11:14:35 +0300

    fixed method logic

--- sorting/array_chunk/array_chunk.py
+++ sorting/array_chunk/array_chunk.py
@@ -1,4 +1,4 @@
-def ArrayChunk(M):
+def ArrayChunk(M: list[int]) -> int:
     if not M:
         return 0
 
@@ -8,25 +8,24 @@ def ArrayChunk(M):
 
         i1 = 0
         i2 = len(M) - 1
-        while M[i1] < N:
-            i1 += 1
 
-        while M[i2] > N:
-            i2 -= 1
+        while True:
+            while M[i1] < N:
+                i1 += 1
 
-        if i1 == i2 - 1 and M[i1] > M[i2]:
+            while M[i2] > N:
+                i2 -= 1
+
+            if i1 == i2 - 1 and M[i1] > M[i2]:
+                M[i1], M[i2] = M[i2], M[i1]
+                break
+
+            if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
+                return N_index
+
             M[i1], M[i2] = M[i2], M[i1]
-            continue
 
-        if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
-            return N_index
-
-        M[i1], M[i2] = M[i2], M[i1]
-
-        if i1 == N_index:
-            N_index = i2
-        elif i2 == N_index:
-            N_index = i1
-
-        i1 += 1
-        i2 -= 1
+            if i1 == N_index:
+                N_index = i2
+            elif i2 == N_index:
+                N_index = i1
