457924c2616b6a1ca3fe599d18baeda163ef5d11
Kirill <kirillgaynov@gmail.com>
2024-08-02 09:00:03 +0300

    implemented quick sort algorithm with tail recursion

---
+++ sorting/quick_sort_with_tail_recursion/quick_sort_tail_optimization.py
@@ -0,0 +1,43 @@
+def ArrayChunk(M: list[int], left: int, right: int) -> int:
+    if not M:
+        return 0
+
+    while True:
+        N_index = (left + right) // 2
+        N = M[N_index]
+
+        i1 = left
+        i2 = right
+
+        while True:
+            while M[i1] < N:
+                i1 += 1
+
+            while M[i2] > N:
+                i2 -= 1
+
+            if i1 == i2 - 1 and M[i1] > M[i2]:
+                M[i1], M[i2] = M[i2], M[i1]
+                break
+
+            if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
+                return N_index
+
+            M[i1], M[i2] = M[i2], M[i1]
+
+            if i1 == N_index:
+                N_index = i2
+            elif i2 == N_index:
+                N_index = i1
+
+
+def QuickSortTailOptimization(M: list[int], left: int, right: int) -> list[int]:
+    while left < right:
+        pi = ArrayChunk(M, left, right)
+        if pi - left < right - pi:
+            QuickSortTailOptimization(M, left, pi - 1)
+            left = pi + 1
+        else:
+            QuickSortTailOptimization(M, pi + 1, right)
+            right = pi - 1
+    return M
