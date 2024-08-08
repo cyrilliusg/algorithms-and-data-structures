750b7893a80436da8e0e59bbc801326216ea6e9f
Kirill <kirillgaynov@gmail.com>
2024-08-08 10:41:58 +0300

    implemented kth_order_statistics step

---
+++ sorting/kth_order_statistics/kth_order_statistics_step.py
@@ -0,0 +1,45 @@
+def ArrayChunk(M: list[int], left: int, right: int) -> int:
+    if not M:
+        return 0
+
+    while True:
+        N_index = (left + right) // 2
+        N = M[N_index]
+
+        i1 = left
+        i2 = right
+
+        while True:
+            while M[i1] < N:
+                i1 += 1
+
+            while M[i2] > N:
+                i2 -= 1
+
+            if i1 == i2 - 1 and M[i1] > M[i2]:
+                M[i1], M[i2] = M[i2], M[i1]
+                break
+
+            if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
+                return N_index
+
+            M[i1], M[i2] = M[i2], M[i1]
+
+            if i1 == N_index:
+                N_index = i2
+            elif i2 == N_index:
+                N_index = i1
+
+
+def KthOrderStatisticsStep(Array: list, L: int, R: int, k: int) -> list[int]:
+    while L <= R:
+        N = ArrayChunk(Array, L, R)
+
+        if N == k:
+            break
+        elif N < k:
+            L = N + 1
+        else:
+            R = N - 1
+
+    return [L, R]
