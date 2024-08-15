86de3d394e6e8979834b94c638d7c4a44878d8f3
cyrilliusg <kirillgaynov@gmail.com>
2024-08-15 12:34:50 +0300

    implemented binary search class

---
+++ sorting/binary_search/binary_search.py
@@ -0,0 +1,33 @@
+class BinarySearch:
+    def __init__(self, sorted_array: list[int]):
+        self.array: list[int] = sorted_array
+        self.L = 0
+        self.R = len(sorted_array) - 1
+        self.result = None
+
+    def Step(self, N: int):
+        if self.result is not None:
+            return
+
+        middle_index = (self.L + self.R) // 2
+
+        if self.array[middle_index] == N:
+            self.result = True
+            return
+
+        if self.L == self.R or abs(self.L - self.R) == 1:
+            self.result = False
+            return
+
+        if self.array[middle_index] < N:
+            self.L = middle_index + 1
+        elif self.array[middle_index] > N:
+            self.R = middle_index - 1
+
+    def GetResult(self) -> int:
+        if self.result is None:
+            return 0
+        elif self.result:
+            return 1
+        else:
+            return -1
