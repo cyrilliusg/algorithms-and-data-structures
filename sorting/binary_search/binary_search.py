cdc54d12db467a10e0a77d0e5cc9d5724692e5ac
cyrilliusg <kirillgaynov@gmail.com>
2024-08-21 23:40:17 +0300

    changed attributes names

--- sorting/binary_search/binary_search.py
+++ sorting/binary_search/binary_search.py
@@ -1,28 +1,28 @@
 class BinarySearch:
     def __init__(self, sorted_array: list[int]):
         self.array: list[int] = sorted_array
-        self.L = 0
-        self.R = len(sorted_array) - 1
+        self.Left = 0
+        self.Right = len(sorted_array) - 1
         self.result = None
 
     def Step(self, N: int):
         if self.result is not None:
             return
 
-        if self.L > self.R:
+        if self.Left > self.Right:
             self.result = False
             return True
 
-        middle_index = (self.L + self.R) // 2
+        middle_index = (self.Left + self.Right) // 2
 
         if self.array[middle_index] == N:
             self.result = True
             return
 
         if self.array[middle_index] < N:
-            self.L = middle_index + 1
+            self.Left = middle_index + 1
         elif self.array[middle_index] > N:
-            self.R = middle_index - 1
+            self.Right = middle_index - 1
 
     def GetResult(self) -> int:
         if self.result is None:
