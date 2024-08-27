da5fb8d375c79d00f8403154d741a0744f6dac6a
cyrilliusg <kirillgaynov@gmail.com>
2024-08-27 22:25:49 +0300

    the GallopingSearch method has been removed as an independent method

--- sorting/galloping_binary_search/binary_search.py
+++ sorting/galloping_binary_search/binary_search.py
@@ -38,35 +38,36 @@ class BinarySearch:
         else:
             return -1
 
-    def GallopingSearch(self, sorted_array: list[int], target: int) -> bool:
-        self.array = sorted_array
-        if not sorted_array:
-            return False
-        i = 1
-        current_index = 0
 
-        while i < len(sorted_array):
-            if sorted_array[current_index] == target:
-                return True
+def GallopingSearch(sorted_array: list[int], target: int) -> bool:
+    if not sorted_array:
+        return False
+    i = 1
+    current_index = 0
 
-            if sorted_array[current_index] < target:
-                i += 1
-                new_index = 2 ** i - 2
-                if new_index < len(sorted_array):
-                    current_index = new_index
-                    continue
-                else:
-                    current_index = len(sorted_array) - 1
-                    break
+    while i < len(sorted_array):
+        if sorted_array[current_index] == target:
+            return True
 
+        if sorted_array[current_index] < target:
+            i += 1
+            new_index = 2 ** i - 2
+            if new_index < len(sorted_array):
+                current_index = new_index
+                continue
             else:
-                lower_bound = (2 ** (i - 1) - 2) + 1
-                upper_bound = current_index
+                current_index = len(sorted_array) - 1
+                break
 
-                self.Left = lower_bound
-                self.Right = upper_bound
-                while self.GetResult() == 0:
-                    self.Step(target)
-                return self.GetResult() == 1
+        else:
+            lower_bound = (2 ** (i - 1) - 2) + 1
+            upper_bound = current_index
 
-        return self.array[current_index] == target
+            binary_search = BinarySearch(sorted_array)
+            binary_search.Left = lower_bound
+            binary_search.Right = upper_bound
+            while binary_search.GetResult() == 0:
+                binary_search.Step(target)
+            return binary_search.GetResult() == 1
+
+    return sorted_array[current_index] == target
