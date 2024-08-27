94e33a0faec638c32f38cd6944eb1ab4edf997f6
cyrilliusg <kirillgaynov@gmail.com>
2024-08-27 09:26:37 +0300

    implemented galloping binary search class and test cases

---
+++ sorting/galloping_binary_search/binary_search.py
@@ -0,0 +1,72 @@
+class BinarySearch:
+    def __init__(self, sorted_array: list[int]):
+        self.array: list[int] = sorted_array
+        self.Left = 0
+        self.Right = len(sorted_array) - 1
+        self.result = None
+
+    def Step(self, N: int):
+        if self.result is not None:
+            return
+
+        if self.Left > self.Right:
+            self.result = False
+            return True
+
+        middle_index = (self.Left + self.Right) // 2
+
+        if self.array[middle_index] == N:
+            self.result = True
+            return
+
+        if self.array[middle_index] < N:
+            self.Left = middle_index + 1
+        elif self.array[middle_index] > N:
+            self.Right = middle_index - 1
+
+        if self.Left == self.Right or self.Left == self.Right - 1:
+            if self.array[self.Left] == N or (self.Left != self.Right and self.array[self.Right] == N):
+                self.result = True
+            else:
+                self.result = False
+
+    def GetResult(self) -> int:
+        if self.result is None:
+            return 0
+        elif self.result:
+            return 1
+        else:
+            return -1
+
+    def GallopingSearch(self, sorted_array: list[int], target: int) -> bool:
+        if not sorted_array:
+            return False
+        i = 1
+        current_index = 0
+
+        while i < len(sorted_array):
+            if sorted_array[current_index] == target:
+                return True
+
+            if sorted_array[current_index] < target:
+                i += 1
+                new_index = 2 ** i - 2
+                if new_index < len(sorted_array):
+                    current_index = new_index
+                    continue
+                else:
+                    current_index = len(sorted_array) - 1
+                    break
+
+            else:
+                lower_bound = (2 ** (i - 1) - 2) + 1
+                upper_bound = current_index
+
+                binary_search = BinarySearch(sorted_array)
+                binary_search.Left = lower_bound
+                binary_search.Right = upper_bound
+                while binary_search.GetResult() == 0:
+                    binary_search.Step(target)
+                return binary_search.GetResult() == 1
+
+        return sorted_array[current_index] == target
---
+++ sorting/galloping_binary_search/unit_test.py
@@ -0,0 +1,64 @@
+import unittest
+
+from sorting.galloping_binary_search.binary_search import BinarySearch
+
+
+class TestGallopingSearch(unittest.TestCase):
+
+    def test_element_at_start(self):
+        # Элемент находится в начале массива
+        search = BinarySearch([])
+        self.assertTrue(search.GallopingSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 1))
+
+    def test_element_at_end(self):
+        # Элемент находится в конце массива
+        search = BinarySearch([])
+        self.assertTrue(search.GallopingSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9))
+
+    def test_element_in_middle(self):
+        # Элемент находится в середине массива
+        search = BinarySearch([])
+        self.assertTrue(search.GallopingSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5))
+
+    def test_element_not_found(self):
+        # Элемент отсутствует в массиве
+        search = BinarySearch([])
+        self.assertFalse(search.GallopingSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10))
+
+    def test_large_array(self):
+        # Поиск в большом массиве
+        search = BinarySearch([])
+        large_array = list(range(1, 10001))
+        self.assertTrue(search.GallopingSearch(large_array, 9999))
+        self.assertFalse(search.GallopingSearch(large_array, 10001))
+
+    def test_single_element_array_found(self):
+        # Массив из одного элемента, элемент найден
+        search = BinarySearch([])
+        self.assertTrue(search.GallopingSearch([5], 5))
+
+    def test_single_element_array_not_found(self):
+        # Массив из одного элемента, элемент не найден
+        search = BinarySearch([])
+        self.assertFalse(search.GallopingSearch([5], 10))
+
+    def test_two_elements_array(self):
+        # Массив из двух элементов
+        search = BinarySearch([])
+        self.assertTrue(search.GallopingSearch([3, 8], 8))
+        self.assertFalse(search.GallopingSearch([3, 8], 5))
+
+    def test_repeated_elements(self):
+        # Массив с повторяющимися элементами
+        search = BinarySearch([])
+        self.assertTrue(search.GallopingSearch([1, 1, 1, 1, 2, 3, 3, 3, 4, 5], 3))
+        self.assertFalse(search.GallopingSearch([1, 1, 1, 1, 2, 3, 3, 3, 4, 5], 6))
+
+    def test_empty_array(self):
+        # Пустой массив
+        search = BinarySearch([])
+        self.assertFalse(search.GallopingSearch([], 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
